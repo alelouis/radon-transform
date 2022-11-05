dca92b3ccc9660a25e71add58eb49531c702d60d
alelouis <me@alelouis.eu>
2022-11-05 13:35:41 +0100

    Adds mat and vec csv writers

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ mod radon;
 fn main() {
     let n_rows = 512_usize;
     let n_cols = 512_usize;
-    let n_rays = 200_usize;
-    let n_slopes = 200_usize;
+    let n_rays = 1000_usize;
+    let n_slopes = 1000_usize;
 
-    let input_path = "lena.csv".to_string();
+    let input_path = "inputs/lena.csv".to_string();
     let image = csv_rw::read_csv(input_path).expect("Error reading input.");
     let radon_transform = radon::radon(&image, n_rows, n_cols, n_rays, n_slopes);
-    csv_rw::write_csv(radon_transform, "rust_output.csv".to_string())
+    csv_rw::write_mat_csv(&radon_transform, "outputs/radon_transform.csv".to_string())
         .expect("Error writing output csv.");
 }
