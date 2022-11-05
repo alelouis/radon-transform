a7054d1c357ed54d9bc5283a1b3e44ac48325824
alelouis <me@alelouis.eu>
2022-11-05 13:35:20 +0100

    Moves mean and variance in array

--- src/array.rs
+++ src/array.rs
@@ -3,3 +3,15 @@ pub fn linear_space(start: f32, stop: f32, steps: i32) -> Vec<f32> {
     let dx = (stop - start) / (steps as f32 - 1.0);
     (0..steps).map(|x| start + x as f32 * dx).collect()
 }
+
+/// Compute mean of vector
+#[allow(dead_code)]
+pub fn mean(array: &Vec<f32>) -> f32 {
+    array.iter().sum::<f32>() / array.len() as f32
+}
+
+/// Compute variance of vector
+#[allow(dead_code)]
+pub fn var(array: &Vec<f32>) -> f32 {
+    array.iter().map(|x| x.powi(2)).sum::<f32>() / array.len() as f32
+}
